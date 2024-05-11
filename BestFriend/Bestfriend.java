fa6c91bffdd31e3cbe451d8787eae35e990eea35
Agapitus <89057298+Agapitus1@users.noreply.github.com>
2024-05-11 18:59:04 +1000

    Add files via upload

---
+++ BestFriend/Bestfriend.java
@@ -0,0 +1,19 @@
+class Bestfriend
+{
+    String Name; 
+    String Age;
+    String Faculty;
+
+    Bestfriend()
+    {
+        Name = "Steven";
+        Age = "19";
+        Faculty = "IT";
+        System.out.println("Loading game into RAM");
+    }
+    void sayHello()
+    {
+        System.out.println("Loading game into RAM");
+    }
+}
+
---
+++ BestFriend/TestBestfriend.java
@@ -0,0 +1,18 @@
+class TestBestfriend
+{
+   
+    Bestfriend bestfriend1, bestfriend2;
+    
+    
+        TestBestfriend()
+        {
+           
+            bestfriend1 = new Bestfriend();
+            bestfriend2 = new Bestfriend();
+            
+            
+         
+            bestfriend1.sayHello();
+            bestfriend2.sayHello();
+	}
+}
